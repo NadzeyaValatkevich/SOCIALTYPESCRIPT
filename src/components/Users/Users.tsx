b6d40a0d3140c2bbfb5a76d53eac8563b06803eb
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2022-04-17 21:08:34 +0300

    added 52 lesson: side effect

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,16 +6,19 @@ import userPhoto from "../../assets/images/user_png.png"
 
 export const Users = (props: UsersPropsType) => {
 
-if(props.usersPage.users.length === 0) {
+    const getUsers = () => {
+        if(props.usersPage.users.length === 0) {
 
-    axios.get("https://social-network.samuraijs.com/api/1.0/users")
-        .then(response => {
-            debugger;
-            props.setUsers(response.data.items)
-        })
-}
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    debugger;
+                    props.setUsers(response.data.items)
+                })
+        }
+    }
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {props.usersPage.users.map(u => <div key={u.id}>
                 <span>
                     <div>
